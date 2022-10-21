9a097f4c38886d8f13fc1f715a357efdfd024d07
Arnol Fokam <arnolfokam23@gmail.com>
2022-10-21 12:11:06 +0200

    finish dataset

--- bow/dataset.py
+++ bow/dataset.py
@@ -1,11 +1,13 @@
 import os
 import math
 import glob
+import pickle
 import logging
 from PIL import Image
 from typing import List
 from collections import defaultdict
 
+import cv2
 import torch
 import numpy as np
 import shapely.wkt
@@ -24,7 +26,7 @@ logging.basicConfig(level=logging.INFO,
 
 class WadhwaniBollwormDataset(torch.utils.data.Dataset):
     
-    bbox_path = "images_bboxes.csv"
+    bbox_path = "images_bboxes_uncorrupted.csv"
     images_path = "images"
     
     bollworms = ["abw", "pbw"]
@@ -33,6 +35,7 @@ class WadhwaniBollwormDataset(torch.utils.data.Dataset):
     def __init__(
             self,
             root_dir: str,
+            save: bool = True,
             train: bool = True,
             download: bool = False,
             transform: Optional[Callable] = None,
@@ -44,8 +47,20 @@ class WadhwaniBollwormDataset(torch.utils.data.Dataset):
         self.max_cache_length = max_cache_length
         self.class_meta = self.__load_meta_class()
         
-        # get the bounding boxes
-        if self.train:
+        try:
+            
+            logging.info('Loading data from cache...')
+            
+            if self.train:
+                with open(f'{self.root_dir}/cache_{self.bbox_path.split(".")[0]}.cache.pkl', 'rb') as f:
+                    self.bboxes = pickle.load(f)
+                    
+            logging.info('Loaded!')
+                    
+        except (IOError, EOFError, OSError, pickle.PickleError, pickle.UnpicklingError):
+            logging.info('Error occured during cache data loading. Preprocessing data again...')
+            
+            # get the bounding boxes
             self.bboxes = []
             self.targets = []
             
@@ -54,15 +69,25 @@ class WadhwaniBollwormDataset(torch.utils.data.Dataset):
             pbar = tqdm(bboxes_df.groupby("image_id"))
             pbar.set_description("Getting bounding box information...")
             
-            for name, bboxes in pbar:
-                tmp_targets = []
-                tmp_bboxes = []
-                for _, row in bboxes.iterrows():
-                    if not pd.isnull(row['worm_type']):
-                        tmp_targets.append(row['worm_type'])
-                        tmp_bboxes.append(shapely.wkt.loads(row["geometry"]).bounds)
+            if self.train:
+                for name, bboxes in pbar:
+                    tmp_targets = []
+                    tmp_bboxes = []
+                    for _, row in bboxes.iterrows():
+                        if not pd.isnull(row['worm_type']):
+                            tmp_targets.append(row['worm_type'])
+                            tmp_bboxes.append(shapely.wkt.loads(row["geometry"]).bounds)
             
-                self.bboxes.append((name.split(".")[0].split("_")[-1], tmp_bboxes, tmp_targets))
+                    self.bboxes.append((name.split(".")[0].split("_")[-1], tmp_bboxes, tmp_targets))
+            
+            if save:
+                logging.info('Caching data for subsequent use...')
+
+                with open(f'{self.root_dir}/cache_{self.bbox_path.split(".")[0]}.cache.pkl', 'wb') as f:
+                    pickle.dump(self.bboxes, f)
+                    
+                logging.info('Cached!')
+
         
         # save cache for our load on the fly algorithm
         self.cache = {}
@@ -75,12 +100,12 @@ class WadhwaniBollwormDataset(torch.utils.data.Dataset):
         image_id, bboxes, targets = self.bboxes[index]
         image = np.array(self.__get_image_from_id(image_id))
         if self.transform:
-            transformed = self.transform(image=image.astype(np.float32))
-            image = transformed["image"].float()
+            image, bboxes = self.transform(image=image.astype(np.float32), bboxes=bboxes)
+            image = image.float()
         else:
             image = torch.FloatTensor(image)
 
-        return int(image_id), image, field_mask, [int(self.class_meta[target]["loss_label"]) for target in targets] if self.train else torch.empty(len(targets))
+        return image_id, bboxes, [int(self.class_meta[target]["loss_label"]) for target in targets] if self.train else torch.empty(len(targets))
 
     @staticmethod
     def get_class_weights(targets):
@@ -104,15 +129,16 @@ class WadhwaniBollwormDataset(torch.utils.data.Dataset):
             # if image in cache, return the image
             return self.cache[image_id]
         else:
-            with Image.open(f"{self.root_dir}/{self.images_path}/id_{image_id}.jpg") as im:
+            image = cv2.imread(f"{self.root_dir}/{self.images_path}/id_{image_id}.jpg")
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
                 
-                # if max cache length attain, remnove one random element
-                if len(self.cache.keys()) >= self.max_cache_length:
-                    del self.cache[random.choice(self.cache.keys())]
+            # if max cache length attain, remnove one random element
+            if len(self.cache.keys()) >= self.max_cache_length:
+                del self.cache[random.choice(self.cache.keys())]
                 
-                # insert next element
-                self.cache[image_id] = im
-                return self.cache[image_id]
+            # insert next element
+            self.cache[image_id] = image
+            return self.cache[image_id]
 
 
 if __name__ == '__main__':
--- bow/transform.py
+++ bow/transform.py
@@ -7,10 +7,17 @@ import scipy
 
 class BaselineTrainTransform:
     def __init__(self,) -> None:
-        pass
+        self.transform = A.Compose([
+            A.RandomCrop(width=450, height=450),
+            A.HorizontalFlip(p=0.5),
+            A.RandomBrightnessContrast(p=0.2),
+        ], bbox_params=A.BboxParams(format='pascal_vocc'))
 
-    def __call__(self, image: Union[np.ndarray, Image], bbox: List[float]) -> Dict[str,  Union[List[float], Image]]:
-        raise NotImplementedError
+    def __call__(self, image: Union[np.ndarray, Image], bboxes: List[float]) -> Dict[str,  Union[List[float], Image]]:
+        transfromed = self.transform(image=image, bboxes=bboxes)
+        transformed_image = transformed['image']
+        transformed_bboxes = transformed['bboxes']
+        
 
 if __name__ == '__main__':
     pass

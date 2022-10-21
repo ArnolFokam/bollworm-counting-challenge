a3de4a37931260d414321109afc39b2e669df4c8
Arnol Fokam <arnolfokam23@gmail.com>
2022-10-21 11:45:51 +0200

    good code

--- bow/transform.py
+++ bow/transform.py
@@ -11,24 +11,6 @@ class BaselineTrainTransform:
 
     def __call__(self, image: Union[np.ndarray, Image], bbox: List[float]) -> Dict[str,  Union[List[float], Image]]:
         raise NotImplementedError
-        
-        
-class BaselineEvalTransform(BaselineTrainTransform):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        
-        # transform for RGB bands
-        self.rgb_transform = A.Compose([])
-
-        # transform that changes the geometric shape of the image (rotation, translation, etc)
-        self.geometric_transform = A.Compose([
-            RandomFieldAreaCrop(crop_size=self.crop_size), 
-        ])
-    
 
 if __name__ == '__main__':
-    bands = ['B01', 'B02', 'B03', 'B04','B05','B06','B07','B08','B8A', 'B09', 'B11', 'B12']
-    ds = AgriFieldDataset('data/source', bands=bands, transform=BaselineTrainTransform(bands=bands, crop_size=16), train=True)
-    loader = torch.utils.data.DataLoader(ds, batch_size=20, shuffle=False)
-    loader = iter(loader)
-    fids, imgs, masks, targets = next(loader)
+    pass
